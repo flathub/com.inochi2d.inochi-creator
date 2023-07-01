45f0665db16ed7c130e3e0432f4ace20a8377eec
Grillo del Mal <grillo@delmal.cl>
2023-06-30 22:29:56 -0400

    Update new i2d-imgui, i2d-opengl and kra-d sources

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -85,9 +85,11 @@ git clone https://github.com/Inochi2D/vmc-d.git
 git clone https://github.com/Inochi2D/inmath.git
 git clone https://github.com/Inochi2D/psd-d.git
 git clone https://github.com/Inochi2D/fghj.git
-git clone https://github.com/Inochi2D/bindbc-imgui.git
+git clone https://github.com/Inochi2D/i2d-imgui.git
+git clone https://github.com/Inochi2D/i2d-opengl.git
 git clone https://github.com/KitsunebiGames/i18n.git i18n-d
 git clone https://github.com/Inochi2D/dportals.git
+git clone https://github.com/Inochi2D/kra-d.git
 
 # Download gitver and semver
 git clone https://github.com/Inochi2D/gitver.git
